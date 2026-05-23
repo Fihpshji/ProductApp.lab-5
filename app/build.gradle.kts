f381ad4c31da7ca7c8ba1c69a3733df880caa741
Maksym <mkorotkyi@ifkep.ukr.education>
2026-05-24 01:03:41 +0300

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.productapp"
+    namespace = "com.example.korotkyi"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.Korotkyi"
+        applicationId = "com.example.korotkyi"
         minSdk = 26
         targetSdk = 36
         versionCode = 1
